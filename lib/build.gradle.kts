a084592b6c6e5825659d58e05b747a5bbc1278f9
Ulrik Taasti Landberg <ulast@systematic.com>
2023-12-22 10:21:59 +0100

    Upgrade dependencies

---
+++ lib/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testCompileOnly("junit:junit:4.13.2")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
